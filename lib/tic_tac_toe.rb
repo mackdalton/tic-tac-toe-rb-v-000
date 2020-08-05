31843fb003de84f933552a7c2bc2fb2903e7b793
Mack Dalton <mackdalton@gmail.com>
2020-08-05 20:17:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,8 +37,9 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) == false 
-    elsif index.between?(0,8) == true
+  if position_taken?(board, index)
+    false
+    elsif index.between?(0,8) 
     true
   else
     false
