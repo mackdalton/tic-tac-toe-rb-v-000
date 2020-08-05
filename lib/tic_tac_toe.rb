22efb07671efbd22e91010e07d3a4199098826a8
Mack Dalton <mackdalton@gmail.com>
2020-08-05 20:53:33 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index) == true
-    move(board, index, current_player)
+    move(board, index, current_player(board))
   else
     "Please try again"
     input = gets.strip
